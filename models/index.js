495aa3314611d09425bf53484831d8f17ae9d0b3
Ernesto Celis <ernesto@celisdelafuente.net>
2022-06-22 16:25:50 -0500

    Fix PostgreSQL connection for Azure

--- models/index.js
+++ models/index.js
@@ -9,11 +9,12 @@ const env = process.env.NODE_ENV || 'development';
 const db = {};
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,
+  process.env.POSTGRES_USER, 
+  process.env.POSTGRES_PASSWORD,
   {
     host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
-    ssl: true
+    dialectOptions: {ssl: true}
   });
 
 fs
