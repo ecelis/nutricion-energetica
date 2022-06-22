cba59d9553836ef0554f8028ffea210a6081dc0e
Ernesto Celis <ernesto@celisdelafuente.net>
2022-06-22 16:01:51 -0500

    Fix PostgreSQL ssl

--- models/index.js
+++ models/index.js
@@ -12,7 +12,8 @@ const sequelize = new Sequelize(process.env.POSTGRES_DB,
   process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,
   {
     host: process.env.POSTGRES_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    ssl: true
   });
 
 fs
